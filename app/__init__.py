bfcd6f0aab234d7908fcf8325049e1c592e022ea
jshe2304 <jshe2304@gmail.com>
2021-07-11 18:52:44 -0700

    upload dowload folder

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,13 @@ import os
 application = app = Flask(__name__)
 app.config.from_object(Config)
 
-#if (os.path.exists())
-#os.mkdir(app.config['UPLOAD_FOLDER'])
+if (not os.path.exists(app.config['CLIENT_FOLDER'])):
+    os.mkdir(app.config['CLIENT_FOLDER'])
+
+if (not os.path.exists(app.config['UPLOAD_FOLDER'])):
+    os.mkdir(app.config['UPLOAD_FOLDER'])
+
+if (not os.path.exists(app.config['DOWNLOAD_FOLDER'])):
+    os.mkdir(app.config['DOWNLOAD_FOLDER'])
 
 from app import routes
--- config.py
+++ config.py
@@ -5,8 +5,9 @@ class Config (object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "6^0)1!0)5%1!4$1!1!9("
 
     abspath = dirname(realpath(__file__))
-    UPLOAD_FOLDER = join(abspath, "app/client/upload")
-    DOWNLOAD_FOLDER = join(abspath, "app/client/download")
+    CLIENT_FOLDER = join(abspath, "app/client")
+    UPLOAD_FOLDER = join(CLIENT_FOLDER, "upload")
+    DOWNLOAD_FOLDER = join(CLIENT_FOLDER, "download")
 
     ALLOWED_EXTENSIONS = ['docx', 'pdf', 'txt']
     MAX_CONTENT_LENGTH = 20 * 1024 * 1024
